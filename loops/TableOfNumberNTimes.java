f4bd51cfcd1938fd621bd06434cddb05c09f48ad
Mradul-Lakhotiya <142927820+Mradul-Lakhotiya@users.noreply.github.com>
2023-11-20 18:39:24 +0530

    20 Nov 2023

--- loops/TableOfNumberNTimes.java
+++ loops/TableOfNumberNTimes.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class TableOfNumberNTimes {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num, times;
+
+        System.out.print("Enter the number for the table : ");
+        num = sc.nextInt();
+
+        System.out.print("Enter the number till you want the table : ");
+        times = sc.nextInt();
+
+        for (int i = 1; i <= times; i++) {
+            System.out.println(i + " x " + num + " = " + num * i);
+        }
+    }
 }
